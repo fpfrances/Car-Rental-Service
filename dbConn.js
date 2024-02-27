734b325f88192cd436161b3264cb80b08d2d134c
Ryan Pettit <rpettit088@gmail.com>
2024-02-27 11:52:36 -0500

    moved mongodb connection back to main dbConn.js file

--- dbConn.js
+++ dbConn.js
@@ -1,11 +1,13 @@
 const mongoose = require ('mongoose');
+require('dotenv').config();
+
 
 const connectDB = async() => {
     try {
         await mongoose.connect(process.env.DATABASE_URI);
-        //console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB');
     }catch (err){
-        console.error(err);
+        console.error('MongoDB connection error:', err);
     }
 
 }
